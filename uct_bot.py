a9614778de42df95b0f5b3950e96009fd78087ba
Alexander Buc <abuc@ucsc.edu>
2015-04-10 13:31:44 -0700

    shit is almost working i think

--- uct_bot.py
+++ uct_bot.py
@@ -56,11 +56,16 @@ class Node:
              s += str(c) + "\n"
         return s
 
-def outcome(score):
-    return
 
 def think(state, quip):
     rootnode = Node(state = state.copy)
+    me = state.get_whos_turn()
+    def outcome(score):
+        if me == 'red':
+            return score['red'] - score['blue']
+        else:
+            return score['blue'] - score['red']
+
 
     for i in range(100):
         node = rootnode
@@ -82,39 +87,3 @@ def think(state, quip):
             node.Update(stateCopy)
     return
 
-def UCT(rootstate, itermax, verbose = False):
-    """ Conduct a UCT search for itermax iterations starting from rootstate.
-        Return the best move from the rootstate.
-        Assumes 2 alternating players (player 1 starts), with game results in the range [0.0, 1.0]."""
-
-    rootnode = Node(state = rootstate)
-
-    for i in range(itermax):
-        node = rootnode
-        state = rootstate.copy()
-
-        # Select
-        while node.untriedMoves == [] and node.childNodes != []: # node is fully expanded and non-terminal
-            node = node.UCTSelectChild()
-            state.apply_move(node.move)
-
-        # Expand
-        if node.untriedMoves != []: # if we can expand (i.e. state/node is non-terminal)
-            m = random.choice(node.untriedMoves)
-            state.apply_move(m)
-            node = node.AddChild(m,state) # add child and descend tree
-
-        # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
-        while state.get_moves() != []: # while state is non-terminal
-            state.apply_move(random.choice(state.apply_move()))
-
-        # Backpropagate
-        while node != None: # backpropagate from the expanded node and work back to the root node
-            node.Update(state.get_score(node.playerJustMoved)) # state is terminal. Update node with result from POV of node.playerJustMoved
-            node = node.parentNode
-
-    # Output some information about the tree - can be omitted
-    if (verbose): print rootnode.TreeToString(0)
-    else: print rootnode.ChildrenToString()
-
-    return sorted(rootnode.childNodes, key = lambda c: c.visits)[-1].move # return the move that was most visited
